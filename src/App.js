6105e90047ceab2c86fa486cd8986b32e8442f66
nohestmm <nohestmm@gmail.com>
2019-08-04 18:53:13 -0400

    adding awesome npm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import photo from './img/photo.jpeg';
+import '../node_modules/@fortawesome/fontawesome-free/js/all'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
 
 class App extends Component {
   render() {
@@ -10,11 +13,22 @@ class App extends Component {
         </div>
         <header>
           <nav className="navbar">
+            <span className="navbar-text"><i className="fas fa-home"></i>
+              Home
+            </span>
+            <span className="navbar-text"> 
+              <i className="fas fa-tools"></i>
+              Skills
+              </span>
+            <span className="navbar-text">
+              <i className="fas fa-briefcase"></i>
+              Portfolio
+                </span>
           </nav>
         </header>
         <section className="bottom-div">
           <h1 className="name">NOHEMÍ MARTÍNEZ</h1>
-          <h6 className= "text-bottom_name">{"<Front-end Developer/>"}</h6>
+          <h6 className="text-bottom_name">{"<Front-end Developer/>"}</h6>
           <p className="paragraph">La programación siempre ha formado parte de mi formación académica, y Laboratoria me brindó la oportunidad de mejorar mis habilidades y aprender Javascript, lo que me ha permitido trabajar en equipo, empleando metodologias ágiles en un ambiente de incertidumbre.</p>
         </section>
         <div className="profile-photo"><img className="myphoto" src={photo} alt="" /></div>
