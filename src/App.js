26547ee61778b8e5121682f5f300ada129818530
nohestmm <nohestmm@gmail.com>
2019-08-04 23:59:02 -0400

    changing css and adding footer icons

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <>
-       
-        <header className= "top-div">
+
+        <header className="top-div">
           <nav className="navbar">
             <span className="navbar-text"><i className="fas fa-home"></i>
               Home
@@ -23,15 +23,20 @@ class App extends Component {
               <i className="fas fa-briefcase"></i>
               Portfolio
                 </span>
+                <span className="navbar-text"><i className="fas fa-bars"></i></span>
           </nav>
         </header>
-        <div className="profile-photo"><img className="myphoto" src={photo} alt="" /></div>
+        <section className="profile-photo"><img className="myphoto" src={photo} alt="" /></section>
         <section className="bottom-div">
           <h1 className="name">NOHEMÍ MARTÍNEZ</h1>
           <h6 className="text-bottom_name">{"<Front-end Developer/>"}</h6>
           <p className="paragraph">La programación siempre ha formado parte de mi formación académica, y Laboratoria me brindó la oportunidad de mejorar mis habilidades y aprender Javascript, lo que me ha permitido trabajar en equipo, empleando metodologias ágiles en un ambiente de incertidumbre.</p>
         </section>
-        <footer>
+        <footer className= "footer">
+          <span><i className="fab fa-github"></i></span>
+          <span><i className="fab fa-facebook"></i></span>
+          <span><i className="fab fa-linkedin-in"></i></span>
+          <span><i className="fas fa-file-download"></i></span>
         </footer>
       </>
     );
