7133d8522bb2dea3ef7b48a8831d6fdc31ff96a2
nohestmm <nohestmm@gmail.com>
2019-08-26 19:24:44 -0300

    adding Link to menu

--- src/components/Navbarmain.jsx
+++ src/components/Navbarmain.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navbarmain = ({ show, hidden }) => {
   const navbar = show ? "navbar hidden-bars" : "navbar show-bars";
   return (
     <nav className={navbar}>
       <div className="content-options">
-       <div className="icon-menu">
-         <span className="navbar-text">
-          <i className="fas fa-home"></i>
-          Home
+        <Link to={"/"}>
+          <div className="icon-menu">
+            <span className="navbar-text">
+              <i className="fas fa-home"></i>
+              Home
             </span>
-            </div>
+          </div>
+        </Link>
+        <Link to={"/skills"}>
+          <div className="icon-menu">
+            <span className="navbar-text">
+              <i className="fas fa-tools"></i>
+              Skills
+              </span>
+          </div>
+        </Link>
+        <Link to={"/projects"}>
         <div className="icon-menu">
           <span className="navbar-text">
-          <i className="fas fa-tools"></i>
-          Skills
-              </span>
-              </div>
-         <div className="icon-menu">
-          <span className="navbar-text">
-          <i className="fas fa-briefcase"></i>
-          Portfolio
-                </span></div>
-               
+            <i className="fas fa-briefcase"></i>
+            Portfolio
+                </span>
+                </div>
+                </Link>
         <div className="icon-menu">
           <span className="navbar-text" onClick={hidden}>
             <i className="fas fa-bars"></i>
-            </span>
-            </div>
+          </span>
+        </div>
       </div>
     </nav>
   )
