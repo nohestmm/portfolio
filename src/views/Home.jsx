3784cd277c26cd0d69ae8254ae9a9e84d133e369
nohestmm <nohestmm@gmail.com>
2019-08-30 17:03:01 -0300

    adding ccs to projects template

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -20,10 +20,10 @@ const Home =({active})=>{
       ágiles en un ambiente de incertidumbre.</p>
   </section>
   <footer className="footer">
-    <a href="https://github.com/nohestmm?tab=repositories" target="blank"><i className="fab fa-github"></i></a>
-    <a href="mailto:nohestmm@gmail.com" target="blank"><i className="fas fa-envelope"></i></a>
-    <a href="https://www.linkedin.com/in/nohestmm/" target="blank"><i className="fab fa-linkedin-in"></i></a>
-    <a href="https://drive.google.com/uc?id=10dn07QmIAxYo2HB3KrNwUUW5QRIfpsiY&export=download&authuser=0" target="blank"><i className="fas fa-file-download"></i></a>
+    <a className ="footer-ancle" href="https://github.com/nohestmm?tab=repositories" target="blank"><i className="fab fa-github"></i></a>
+    <a className ="footer-ancle" href="mailto:nohestmm@gmail.com" target="blank"><i className="fas fa-envelope"></i></a>
+    <a className ="footer-ancle" href="https://www.linkedin.com/in/nohestmm/" target="blank"><i className="fab fa-linkedin-in"></i></a>
+    <a className ="footer-ancle" href="https://drive.google.com/uc?id=10dn07QmIAxYo2HB3KrNwUUW5QRIfpsiY&export=download&authuser=0" target="blank"><i className="fas fa-file-download"></i></a>
   </footer>
  </> 
  )
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -6,25 +6,25 @@ import moviesproject from '../img/moviesproject.jpg'
 import pinterest from '../img/pinterest.jpg'
 
 
-const Projects = ({active}) =>{
-   
- 
-    return( 
-        <>
+const Projects = ({ active }) => {
 
-        <Header main={active}/>
+
+  return (
+    <>
+
+      <Header main={active} />
       <h1 className="title-otherviews">Projects</h1>
       <section className="content-project">
-        <div><img src={mdlinks} alt="mdlinks"/></div>
-        <div><img src={pokedata} alt="pokedata"/></div>
-        <div><img src={moviesproject} alt="movies"/></div>
-        <div><img src={pinterest} alt="pinterest"/></div>
+        <div className="content-details"><img className="img-projects" src={pokedata} alt="pokedata" /></div>
+        <div className="content-details"><img className="img-projects" src={moviesproject} alt="movies" /></div>
+        <div className="content-details"><img className="img-projects" src={mdlinks} alt="mdlinks" /></div>
+        <div className="content-details"><img className="img-projects" src={pinterest} alt="pinterest" /></div>
       </section>
-</>
-  
-       
+    </>
 
- )
-    
+
+
+  )
+
 }
 export default Projects;
