b44571510c16f62445be39345b2e5c34e21c52d0
nohestmm <nohestmm@gmail.com>
2019-09-01 17:05:08 -0300

    adding css to projects for preview

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -4,35 +4,54 @@ import mdlinks from '../img/mdlinks.jpg'
 import pokedata from '../img/pokedata.jpg'
 import moviesproject from '../img/moviesproject.jpg'
 import pinterest from '../img/pinterest.jpg'
-
-
 const Projects = ({ active }) => {
-
-
   return (
     <>
-
       <Header main={active} />
       <h1 className="title-otherviews">Projects</h1>
       <section className="content-project">
-        <div className="content-details" onClick={() => this.showpreview(this.state.show)}>
-          <img className="img-projects" src={pokedata} alt="pokedata" />
+        <div className="content-rawprojects">
+          <div className="content-iconprojects">
+            <div className="content-projectsdetails" onClick={() => this.showpreview(this.state.show)}>
+              <img className="img-projects" src={pokedata} alt="pokedata" />
+            </div>
+            <div className="icons-bottomproject">
+            <span className="rombo-icon"><i className="fas fa-link"></i></span>
+            <span className="rombo-icon"><i className="fas fa-search"></i></span>
+            </div>
+          </div>
+          <div className="content-iconprojects">
+            <div className="content-projectsdetails">
+              <img className="img-projects" src={moviesproject} alt="movies" />
+            </div>
+            <div className="icons-bottomproject">
+            <span className="rombo-icon"><i className="fas fa-link"></i></span>
+            <span className="rombo-icon"><i className="fas fa-search"></i></span>
+            </div>
+          </div>
         </div>
-        <div className="content-details">
-          <img className="img-projects" src={moviesproject} alt="movies" />
-        </div>
-        <div className="content-details">
-          <img className="img-projects" src={mdlinks} alt="mdlinks" />
-        </div>
-        <div className="content-details">
-          <img className="img-projects" src={pinterest} alt="pinterest" />
+        <div className="content-rawprojects">
+          <div className="content-iconprojects">
+            <div className="content-projectsdetails">
+              <img className="img-projects" src={mdlinks} alt="mdlinks" />
+            </div>
+            <div className="icons-bottomproject">
+              <span className="rombo-icon"><i className="fas fa-link"></i></span>
+              <span className="rombo-icon"><i className="fas fa-search"></i></span>
+            </div>
+          </div>
+          <div className="content-iconprojects">
+            <div className="content-projectsdetails">
+              <img className="img-projects" src={pinterest} alt="pinterest" />
+            </div>
+            <div className="icons-bottomproject">
+            <span className="rombo-icon"><i className="fas fa-link"></i></span>
+            <span className="rombo-icon"><i className="fas fa-search"></i></span>
+            </div>
+          </div>
         </div>
       </section>
     </>
-
-
-
   )
-
 }
 export default Projects;
