70380b60c2baf46e1ce41de0882eee850e4ece71
nohestmm <nohestmm@gmail.com>
2019-09-02 01:43:10 -0300

    adding icon

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-const Preview = ({show, imageProject, hidden}) => {
-  const showPreview= show ? "preview show-times" : "modal hidden-times";
+const Preview = ({ show, imageProject, hidden }) => {
+    const showPreview = show ? "preview show-times" : "modal hidden-times";
 
-  return (
-    <section className={showPreview}>
-      
-         <section className="preview-main"> 
-           <div className= "button-closemodal">
-        <button className = "btn-close " onClick={hidden}>
-            <i className="fas fa-times"></i>
-        </button>
-        </div> 
-          <div className="content-img">
-            <img src={imageProject} alt="" className="img-preview" />
-            </div>
-           </section>
-           </section>
-       
-  );
+    return (
+        <section className={showPreview}>
+            <section className="preview-main">
+                <div className="button-closemodal">
+                    <button className="btn-close " onClick={hidden}>
+                        <i className="fas fa-times"></i>
+                    </button>
+                </div>
+                <div className="content-img">
+                    <img src={imageProject} alt="" className="img-preview" />
+                </div>
+            </section>
+        </section>
+
+    );
 };
 export default Preview;
