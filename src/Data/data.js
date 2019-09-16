5cfb0ba9b5964ea8ab6ddaa5806c59cdc2d0bc2d
nohestmm <nohestmm@gmail.com>
2019-09-16 20:08:48 -0300

    adding data projects

---
+++ src/Data/data.js
@@ -0,0 +1,32 @@
+ const data = [
+        {
+            "title": "Pokedata",
+            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pokedata.jpg?raw=true",
+            "url": "https://nohestmm.github.io/SCL009-data-lovers/src/index.html",
+            "description": "Aplicación que permite conocer la primera generación de pokemones",
+            "github": "https://github.com/nohestmm/SCL009-data-lovers"
+        },
+        {
+            "title": "PeliSagas",
+            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/moviesproject.jpg?raw=true",
+            "url": "https://nohestmm.github.io/SCL009-hackathon-peliculas/src/index.html",
+            "description": "Aplicación que permite conocer sagas de películas",
+            "github": "https://github.com/nohestmm/SCL009-hackathon-peliculas"
+        },
+        {
+            "title": "md-links",
+            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/mdlinks.jpg?raw=true",
+            "url": "https://www.npmjs.com/package/md-links-nohestmm",
+            "description": "Dependencia para leer archvios Markdown",
+            "github": "ttps://github.com/nohestmm/SCL009-md-links"
+        },
+        {
+            "title": "Pokedata",
+            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pinterest.jpg?raw=true",
+            "url": "https://nohestmm.github.io/SCL-009-Pinterest/",
+            "description": "Aplicación que simula la replica del tablero de Pinterest",
+            "github": "https://github.com/nohestmm/SCL-009-Pinterest"
+        }
+    ]
+
+    export default data;
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Preview from '../components/Preview'
-import mdlinks from '../img/mdlinks.jpg'
-import pokedata from '../img/pokedata.jpg'
-import moviesproject from '../img/moviesproject.jpg'
-import pinterest from '../img/pinterest.jpg'
 import Header from '../components/Header'
+import data from '../Data/data'
+
 
 
 class Projects extends React.Component {
- 
+
   constructor(props) {
     super(props)
     this.state = {
@@ -18,113 +16,48 @@ class Projects extends React.Component {
     this.showpreview = this.showpreview.bind(this)
     this.hiddenpreview = this.hiddenpreview.bind(this)
   }
-  showpreview(img){
+  showpreview(img) {
     this.setState({ show: true })
     this.setState({ imagePreview: img })
-    
+
   }
-  hiddenpreview =()=>{
+  hiddenpreview = () => {
     this.setState({ show: false })
   }
   render() {
-    
+
     return (
       <>
-      <Header active = { true } />
+        <Header active={true} />
         <section className="content-project">
           <h1 className="title-SkillProjects">Portfolio</h1>
           <div className="content-rawprojects">
-            <div className="content-iconprojects">
-              <div className="content-projectsdetails">
-                <img className="img-projects" src={pokedata} alt="pokedata" />
-              </div>
-              <div className="icons-bottomproject">
-                <a className="rombo-icon"
-                  href="https://nohestmm.github.io/SCL009-data-lovers/src/index.html"
-                  target="blank"
-                  title="Aplicación que permite conocer la primera generación de pokemones">
-                  <i className="fas fa-link"></i>
-                </a>
-                <span className="rombo-icon" onClick={() => this.showpreview(pokedata)}>
-                  <i className="fas fa-search"></i>
-                </span>
-                <a className="rombo-icon"
-                  href="https://github.com/nohestmm/SCL009-data-lovers"
-                  target="blank"
-                  title="Aplicación que permite conocer la primera generación de pokemones">
-                  <i className="fab fa-github rotate-icon"></i>
-                </a>
-              </div>
-            </div>
-            <div className="content-iconprojects">
-              <div className="content-projectsdetails">
-                <img className="img-projects" src={moviesproject} alt="movies" />
-              </div>
-              <div className="icons-bottomproject">
-                <a className="rombo-icon"
-                  href="https://nohestmm.github.io/SCL009-hackathon-peliculas/src/index.html"
-                  target="blank"
-                  title="Aplicación que permite conocer sagas de películas">
-                  <i className="fas fa-link"></i>
-                </a>
-                <span className="rombo-icon" onClick={() => this.showpreview(moviesproject)}>
-                  <i className="fas fa-search"></i>
-                </span>
-                <a className="rombo-icon"
-                  href="https://github.com/nohestmm/SCL009-hackathon-peliculas"
-                  target="blank"
-                  title="Aplicación que permite conocer la primera generación de pokemones">
-                  <i className="fab fa-github rotate-icon"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="content-rawprojects">
-            <div className="content-iconprojects">
-              <div className="content-projectsdetails">
-                <img className="img-projects" src={mdlinks} alt="mdlinks" />
-              </div>
-              <div className="icons-bottomproject">
-                <a className="rombo-icon"
-                  href="https://www.npmjs.com/package/md-links-nohestmm"
-                  target="blank"
-                  title="Dependencia para leer archvios Markdown">
-                  <i className="fas fa-link"></i>
-                </a>
-                <span className="rombo-icon" onClick={() => this.showpreview(mdlinks)}>
-                  <i className="fas fa-search"></i>
-                </span>
-                <a className="rombo-icon"
-                  href="https://github.com/nohestmm/SCL009-md-links"
-                  target="blank"
-                  title="Aplicación que permite conocer sagas de películas">
-                  <i className="fab fa-github rotate-icon"></i>
-                </a>
-              </div>
-            </div>
-            <div className="content-iconprojects">
-              <div className="content-projectsdetails">
-                <img className="img-projects" src={pinterest} alt="pinterest" />
-              </div>
-              <div className="icons-bottomproject">
-                <a className="rombo-icon"
-                  href="https://nohestmm.github.io/SCL-009-Pinterest/"
-                  target="blank"
-                  title="Replica de Pinterest version desktop">
-                  <i className="fas fa-link"></i>
-                </a>
-                <span className="rombo-icon" onClick={() => this.showpreview(pinterest)}>
-                  <i className="fas fa-search">
-                  </i>
-                </span>
-                <a className="rombo-icon"
-                  href="https://github.com/nohestmm/SCL-009-Pinterest"
-                  target="blank"
-                  title="Aplicación que permite conocer sagas de películas">
-                  <i className="fab fa-github rotate-icon"></i>
-                </a>
-              </div>
-            </div>
+            {
+              data.map((el, index) =>
+                <div className="content-iconprojects" key={index}>
+                  <div className="content-projectsdetails">
+                    <img className="img-projects" src={el.image} alt={el.title} />
+                  </div>
+                  <div className="icons-bottomproject">
+                    <a className="rombo-icon"
+                      href={el.url}
+                      target="blank"
+                      title={el.description}>
+                      <i className="fas fa-link"></i>
+                    </a>
+                    <span className="rombo-icon" onClick={() => this.showpreview(el.image)}>
+                      <i className="fas fa-search"></i>
+                    </span>
+                    <a className="rombo-icon"
+                      href={el.github}
+                      target="blank"
+                      title={el.description}>
+                      <i className="fab fa-github rotate-icon"></i>
+                    </a>
+                  </div>
+                </div>
+              )
+            }
           </div>
           <Preview
             show={this.state.show}
