dde57a2f5090252fbc08d75e31f13000020dc164
nohestmm <nohestmm@gmail.com>
2019-10-08 14:04:47 -0300

    adding trivia project

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,32 +1,39 @@
- const data = [
-        {
-            "title": "Pokedata",
-            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pokedata.jpg?raw=true",
-            "url": "https://nohestmm.github.io/SCL009-data-lovers/src/index.html",
-            "description": "Aplicación que permite conocer la primera generación de pokemones",
-            "github": "https://github.com/nohestmm/SCL009-data-lovers"
-        },
-        {
-            "title": "PeliSagas",
-            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/moviesproject.jpg?raw=true",
-            "url": "https://nohestmm.github.io/SCL009-hackathon-peliculas/src/index.html",
-            "description": "Aplicación que permite conocer sagas de películas",
-            "github": "https://github.com/nohestmm/SCL009-hackathon-peliculas"
-        },
-        {
-            "title": "md-links",
-            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/mdlinks.jpg?raw=true",
-            "url": "https://www.npmjs.com/package/md-links-nohestmm",
-            "description": "Dependencia para leer archvios Markdown",
-            "github": "ttps://github.com/nohestmm/SCL009-md-links"
-        },
-        {
-            "title": "Pokedata",
-            "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pinterest.jpg?raw=true",
-            "url": "https://nohestmm.github.io/SCL-009-Pinterest/",
-            "description": "Aplicación que simula la replica del tablero de Pinterest",
-            "github": "https://github.com/nohestmm/SCL-009-Pinterest"
-        }
-    ]
+const data = [
+    {
+        "title": "Trivia",
+        "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pokedata.jpg?raw=true",
+        "url": "https://trivia-cultural.firebaseapp.com/",
+        "description":"Trivia de preguntas y respuestas acerca de cultura de paises latinos",
+        "github": "https://github.com/nohestmm/trivia"
+    },
+    {
+        "title": "Pokedata",
+        "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pokedata.jpg?raw=true",
+        "url": "https://nohestmm.github.io/SCL009-data-lovers/src/index.html",
+        "description": "Aplicación que permite conocer la primera generación de pokemones",
+        "github": "https://github.com/nohestmm/SCL009-data-lovers"
+    },
+    {
+        "title": "PeliSagas",
+        "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/moviesproject.jpg?raw=true",
+        "url": "https://nohestmm.github.io/SCL009-hackathon-peliculas/src/index.html",
+        "description": "Aplicación que permite conocer sagas de películas",
+        "github": "https://github.com/nohestmm/SCL009-hackathon-peliculas"
+    },
+    {
+        "title": "md-links",
+        "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/mdlinks.jpg?raw=true",
+        "url": "https://www.npmjs.com/package/md-links-nohestmm",
+        "description": "Dependencia para leer archvios Markdown",
+        "github": "ttps://github.com/nohestmm/SCL009-md-links"
+    },
+    {
+        "title": "Pokedata",
+        "image": "https://github.com/nohestmm/portfolio/blob/master/src/img/pinterest.jpg?raw=true",
+        "url": "https://nohestmm.github.io/SCL-009-Pinterest/",
+        "description": "Aplicación que simula la replica del tablero de Pinterest",
+        "github": "https://github.com/nohestmm/SCL-009-Pinterest"
+    }
+]
 
-    export default data;
+export default data;
